21eb2d3bdeb62708d4f5b4f89bbbd58c76dbaf18
Andrew Holmes <acholmes001@gmail.com>
2023-03-08 12:28:57 -0500

    More unit testing

--- pset2/tests/test_unit.py
+++ pset2/tests/test_unit.py
@@ -26,17 +26,59 @@ def test_logical_clock():
     assert(m10.clock == CLOCK_AHEAD + 2)
 
 def test_log():
-    m10 = Machine(0)
-    # Attempt to log a received where we get no data (error, should be clock time of sender)
+    m10 = Machine(10)
+
+    # Test 0: Check for initialization log
+
+    # Test 1: Attempt to log a received where we get no data (error, should be clock time of sender)
     m10.log(MessageType.RECEIVED)
-    # Attempt to log a SENT_ONE where we get no data (error, should be id of which we sent to)
+
+    # Test 2: Attempt to log a SENT_ONE where we get no data (error, should be id of which we sent to)
     m10.log(MessageType.SENT_ONE)
-    # Attempt to log a received with some simulated data
-    m10.log(MessageType.RECEIVED, data = 50)
-    # Attempt to log a SENT_ONE with some real data (i.e. an id):
-    m10.log(MessageType.SENT_ONE, data = 11)
+
+    # Test 3: Attempt to log a received with some simulated
+    RECEIVED_CLOCK = 50
+    m10.log(MessageType.RECEIVED, data = RECEIVED_CLOCK)
+
+    # Test 4: Now let's actually put it in the queue as if we received a message and check the log again
+    # When make_action removes it and logs (should actually update clock appropriately too and log that correctly)
+    m10.queue.put(int.to_bytes(RECEIVED_CLOCK))
+    m10.make_action()
+
+    # Test 5: Attempt to log a SENT_ONE with some real data (i.e. an id):
+    SENDER_ID = 11
+    m10.log(MessageType.SENT_ONE, data = SENDER_ID)
+
+    # Test 6: Attempt to log a SENT_TWO message
+    m10.log(MessageType.SENT_TWO)
+
+    # Test 7: Attempt to log a INTERNAL message
+    m10.log(MessageType.INTERNAL)
 
     with open(f"../logs/log_10.txt", "r") as f:
         logs = f.readlines()
         # Checking for initialization 
         assert "LOG FOR MACHINE 10 WITH CLOCK SPEED" in logs[0]
+        assert "0 - " in logs[1] and "ERROR, Invalid message type." in logs[1]
+        assert "0 - " in logs[2] and "ERROR, Invalid message type." in logs[2]
+        assert "0 - " in logs[3] and f"Received message: {RECEIVED_CLOCK}. Queue length: 0." in logs[3]
+        assert "51 - " in logs[4] and f"Received message: {RECEIVED_CLOCK}. Queue length: 0." in logs[4]
+        assert "51 - " in logs[5] and f"Sent one message to machine {SENDER_ID}." in logs[5]
+        assert "51 - " in logs[6] and "Sent two messages." in logs[6]
+        assert "51 - " in logs[7] and "Internal event." in logs[7]
+
+def test_make_action():
+    m10 = Machine(10)
+    # Test 1: Check whether make_action internal event behaves correctly. 
+    # Note clock should increment as actually made an action
+    m10.make_action(7)
+
+    # Test 2: Check whether make_action behaves correctly with item in queue.
+    RECEIVED_CLOCK = 50
+    m10.queue.put(int.to_bytes(RECEIVED_CLOCK))
+    m10.make_action()
+
+    with open(f"../logs/log_10.txt", "r") as f:
+        logs = f.readlines()
+        assert "1 - " in logs[1] and "Internal event." in logs[1]
+        assert f"{RECEIVED_CLOCK + 1} - " in logs[2] and f"Received message: {RECEIVED_CLOCK}. Queue length: 0." in logs[2]
